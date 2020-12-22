bcbf611a04771f0a5fe0b748319f33e7d15bcc61
KalluruS <>
2020-12-22 08:46:00 +0530

    Update terraform-project/terraform-vpc/

--- gateways.tf
+++ gateways.tf
@@ -7,3 +7,11 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_eip" "ngw" {
+  vpc                 = true
+}
+
+//resource "aws_nat_gateway" "ngw" {
+//  allocation_id       = aws_eip.nat.id
+//  subnet_id           = aws_subnet.example.id
+//}
