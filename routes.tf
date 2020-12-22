55397aa7bcf86ed7f5a0f08ca3fd86eb67a2d520
KalluruS <>
2020-12-22 08:58:10 +0530

    Update terraform-project/terraform-vpc/

--- routes.tf
+++ routes.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table" "public" {
   vpc_id              = aws_vpc.main.id
   route {
-    cidr_block        = "10.0.1.0/24"
+    cidr_block        = "0.0.0.0/0"
     gateway_id        = aws_internet_gateway.igw.id
   }
   tags                = {
@@ -14,7 +14,7 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id              = aws_vpc.main.id
   route {
-    cidr_block        = "10.0.1.0/24"
+    cidr_block        = "0.0.0.0/0"
     nat_gateway_id    = aws_nat_gateway.ngw.id
   }
   tags                = {
