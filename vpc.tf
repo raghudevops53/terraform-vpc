4e2dbcedd052a0d6499a8b4b330d2e5a83a98c97
KalluruS <>
2020-12-24 08:57:52 +0530

    Update terraform-project/terraform-vpc/

--- vpc.tf
+++ vpc.tf
@@ -1,32 +1,34 @@
 resource "aws_vpc" "main" {
-  cidr_block          = var.VPC_CIDR
-  tags                = {
-    Name              = "${var.PROJECT_NAME}-${var.ENV}-vpc"
-    Environment       = var.ENV
-    Provisioned_by    = "Terraform"
+  cidr_block                = var.VPC_CIDR
+  enable_dns_support        = true
+  enable_dns_hostnames      = true
+  tags                      = {
+    Name                    = "${var.PROJECT_NAME}-${var.ENV}-vpc"
+    Environment             = var.ENV
+    Provisioned_by          = "Terraform"
   }
 }
 
 
 resource "aws_subnet" "private-subnets" {
-  count               = 2
-  cidr_block          = cidrsubnet(var.VPC_CIDR, 2, count.index)
-  vpc_id              = aws_vpc.main.id
-  tags                = {
-    Name              = "${var.PROJECT_NAME}-${var.ENV}-private-subnet-${count.index+1}"
-    Environment       = var.ENV
-    Provisioned_by    = "Terraform"
+  count                     = 2
+  cidr_block                = cidrsubnet(var.VPC_CIDR, 2, count.index)
+  vpc_id                    = aws_vpc.main.id
+  tags                      = {
+    Name                    = "${var.PROJECT_NAME}-${var.ENV}-private-subnet-${count.index+1}"
+    Environment             = var.ENV
+    Provisioned_by          = "Terraform"
   }
 }
 
 resource "aws_subnet" "public-subnets" {
-  count               = 2
-  cidr_block          = cidrsubnet(var.VPC_CIDR, 2, count.index+2)
-  vpc_id              = aws_vpc.main.id
-  tags                = {
-    Name              = "${var.PROJECT_NAME}-${var.ENV}-public-subnet-${count.index+1}"
-    Environment       = var.ENV
-    Provisioned_by    = "Terraform"
+  count                     = 2
+  cidr_block                = cidrsubnet(var.VPC_CIDR, 2, count.index+2)
+  vpc_id                    = aws_vpc.main.id
+  tags                      = {
+    Name                    = "${var.PROJECT_NAME}-${var.ENV}-public-subnet-${count.index+1}"
+    Environment             = var.ENV
+    Provisioned_by          = "Terraform"
   }
 }
 
