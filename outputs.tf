889a4e5b390346ac5b326d90f0a5f492e3b8bc94
KalluruS <>
2020-12-24 09:06:51 +0530

    Update terraform-project/terraform-vpc/

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,11 @@ output "VPC_CIDR" {
   value = aws_vpc.main.cidr_block
 }
 
+output "DEFAULT_VPC_ID" {
+  value = var.DEFAULT_VPC_ID
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = var.DEFAULT_VPC_CIDR
+}
+
