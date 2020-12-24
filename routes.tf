cf056a477f4c88348b8d90f823ca1748d98be347
KalluruS <>
2020-12-24 09:04:23 +0530

    Update terraform-project/terraform-vpc/

--- routes.tf
+++ routes.tf
@@ -4,6 +4,10 @@ resource "aws_route_table" "public" {
     cidr_block        = "0.0.0.0/0"
     gateway_id        = aws_internet_gateway.igw.id
   }
+  route {
+    cidr_block                  = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id   = aws_vpc_peering_connection.peering.id
+  }
   tags                = {
     Name              = "${var.PROJECT_NAME}-${var.ENV}-public-rtable"
     Environment       = var.ENV
@@ -16,6 +20,10 @@ resource "aws_route_table" "private" {
   route {
     cidr_block        = "0.0.0.0/0"
     nat_gateway_id    = aws_nat_gateway.ngw.id
+  }
+  route {
+    cidr_block                  = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id   = aws_vpc_peering_connection.peering.id
   }
   tags                = {
     Name              = "${var.PROJECT_NAME}-${var.ENV}-public-rtable"
@@ -34,5 +42,12 @@ resource "aws_route_table_association" "private" {
   count               = 2
   route_table_id      = aws_route_table.private.id
   subnet_id           = element(aws_subnet.private-subnets.*.id, count.index)
+}
+
+## Route to default vpc route table
+resource "aws_route" "default-rt-route" {
+  route_table_id                = var.DEFAULT_RT
+  destination_cidr_block        = aws_vpc.main.cidr_block
+  vpc_peering_connection_id     = aws_vpc_peering_connection.peering.id
 }
 
--- vars.tf
+++ vars.tf
@@ -3,3 +3,4 @@ variable "PROJECT_NAME" {}
 variable "ENV" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_RT" {}
